22f6785b8ad32863e9edbe2c612bf6571d9f525c
Mahtab Alam <mahtab.alam@tech-resource.com>
2018-04-21 12:47:17 +0530

    instafeed file updted

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+# Instagram.configure do |config|
+# 	config.client_id = '23f54db75a07446d883561ae8468bcd7'
+# 	config.client_secret = '890434f3dfa74738b79ffd798df956c2'
+# 	config.access_token = '12321321321312'
+# end
